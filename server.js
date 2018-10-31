56aa61dd095080a2c801754f1fc2248cf057894b
Mario Serrano <maseflores79@gmail.com>
2018-10-30 20:17:36 -0700

    cascareando middlewares

--- server.js
+++ server.js
@@ -1,13 +1,36 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 
 const app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
 
 app.set('view engine', 'hbs');
-app.use(express.static(__dirname + '/public'));
 
+
+// Middleware #1
+app.use((req, res, next) => {
+    const now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err)
+            console.log(err);
+    });
+    /* El next se usa para decirle a express que el método ha terminado. */
+    next();
+    /* Si hacemos algo asíncrono, el middleware no se moverá hasta que se invoque
+    el next */
+});
+
+// Middleware #2 , usado en epoca de mantenimiento
+/*app.use((req, res, next) => {
+    res.render('maintenance.hbs');
+});*/
+
+
+app.use(express.static(__dirname + '/public'));
 /* Al registrar esto, las paginas que tienen los partials no necesitan recibir los 
  parametros que ocupan */
 hbs.registerHelper('currentYear', () => { return new Date().getFullYear(); });
@@ -26,6 +49,13 @@ app.get('/', (req, res) => {
             'Cities'
         ]
     });
+});
+
+app.get('/maintenance', (req, res) => {
+    res.render('maintenance.hbs', {
+        pageTitle: 'Under maintenance!'
+    });
+
 });
 
 app.get('/about', (req, res) => {
